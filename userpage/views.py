4a3ce4389c5f68a0e1c408fc94c8f263de0cea4f
kj F <kj F>
2016-12-28 23:09:45 +0800

    quitconf

--- userpage/views.py
+++ userpage/views.py
@@ -11,23 +11,43 @@ import time
 import json
 
 class JoinConf(APIView):
-    def post(self):
-    	self.check_input('user_id', 'confid', 'type', 'code')
-    	result = joinconf(self.input['user_id'], self.input['confid'], self.input['type'], self.input['code'])
+    def get(self):
+    	self.check_input('user_id', 'confid', 'type')
+    	result = joinconf(self.input['user_id'], self.input['confid'], self.input['type'])
     	return result
 
 
-def joinconf(userid, confid, type, code):
-	urlvalue = urllib.parse.urlencode({"userid": userid, "confid": confid, "type": type, "code": code})
-	postdata = (""), encode('utf-8')
-	url = "http://60.205.137.139/adminweb/REST/APT-V2/joinConf?" + urlvalue
-	request = urllib.request.Request(url.postdata)
-	response = urllib.request.urlopen(request)
-	s = response.read().decode('utf-8')
-	res = json.loads(s)
-	return res
+	def joinconf(userid, confid, type, code):
+		urlvalue = urllib.parse.urlencode({"userid": userid, "confid": confid, "type": type, "code": code})
+		postdata = ("").encode('utf-8')
+		url = "http://60.205.137.139/adminweb/REST/APT-V2/joinConf?" + urlvalue
+		request = urllib.request.Request(url.postdata)
+		response = urllib.request.urlopen(request)
+		s = response.read().decode('utf-8')
+		res = json.loads(s)
+		return res
 
+class CancelConf(APIView):
+	def get(self):
+		result = cancelConf(self.input['user_id'], self.input['confid'])
+		return result
 
+	def cancelConf():
+		urlvalue = urllib.parse.urlencode({"userid": userid, "confid": confid})
+		postdata = ("").encode('utf-8')
+		url = "http://60.205.137.139/adminweb/REST/APT-V2/cancelConf?" + urlvalue
+		request = urllib.request.Request(url.postdata)
+		response = urllib.request.urlopen(request)
+		s = response.read().decode('utf-8')
+		res = json.loads(s)
+		return res
+
+class payDetail(APIView):
+    def get(self):
+        a = {"reason":"", "money":"100w",}
+        return a
+
+        
 class MeetingDetail(APIView):
 
     def get(self):
